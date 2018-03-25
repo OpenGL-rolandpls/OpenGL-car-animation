5d1670f35a094c0be0c2e0796b9a7a69e5582af6
Roland Hartanto <rolandhartanto@gmail.com>
2018-03-26 01:20:46 +0700

    add sky animation

--- src/main_vao.py
+++ src/main_vao.py
@@ -8,6 +8,23 @@ import pygame
 import numpy
 
 transform = 0.0
+skyColorR = 109/255
+skyColorG = 238/255
+skyColorB = 255/255
+
+skyDarkR = 13/255
+skyDarkG = 50/255
+skyDarkB = 73/255
+
+skyR = 109/255
+skyG = 238/255
+skyB = 255/255
+
+plus = False
+
+dR = (- skyDarkR + skyColorR)/1500
+dG = (- skyDarkG + skyColorG)/1500
+dB = (- skyDarkB + skyColorB)/1500
 
 vertex_shader = """
 #version 330
@@ -33,23 +50,31 @@ void main()
 }
 """
 
-fragment_shader2 = """
+fragment_shader_sky = """
 #version 330
+
+uniform float r, g, b;
+
 void main()
 {
-   gl_FragColor = vec4(0.0f, 0.0f, 0.0f, 0.0f);
-    
+   gl_FragColor = vec4(109.0f/255.0f + r, 238.0f/255.0f + g, 255.0f/255.0f + b, 0.0f);
 }
 """
 
-vertices = [ 0.6,  0.6, 0.0, 1.0,
+background = [-1.0, 1.0, 0.0, 1.0,
+              1.0, 1.0, 0.0, 1.0,
+              1.0, -1.0, 0.0, 1.0,
+              -1.0, -1.0, 0.0, 1.0]
+
+vertices = [ 1,  1, 0.0, 1.0,
             0.0,  0.6, 0.0, 1.0,
              0.0, -0.6, 0.0, 1.0]
 
 vertices2 = [ 0.0,  0.6, 0.0, 1.0,
             -0.6,  0.6, 0.0, 1.0,
              0.0, -0.6, 0.0, 1.0]
-			 
+
+background = numpy.array(background, dtype=numpy.float32)			 
 vertices = numpy.array(vertices, dtype=numpy.float32)
 vertices2 = numpy.array(vertices2, dtype=numpy.float32)
 
@@ -72,7 +97,7 @@ def create_object(shader, datas):
     GL.glVertexAttribPointer(position, 4, GL.GL_FLOAT, False, 0, ctypes.c_void_p(0))
     
     # Send the data over to the buffer
-    GL.glBufferData(GL.GL_ARRAY_BUFFER, 48, datas, GL.GL_STATIC_DRAW)
+    GL.glBufferData(GL.GL_ARRAY_BUFFER, 4 * len(datas), datas, GL.GL_STATIC_DRAW)
     #GL.glBufferData(GL.GL_ARRAY_BUFFER, 48, vertices2, GL.GL_STATIC_DRAW)
     # Unbind the VAO first (Important)
     GL.glBindVertexArray( 0 )
@@ -85,6 +110,10 @@ def create_object(shader, datas):
     
 def display(shader, vertex_array_object):
     global transform
+    global dR, dG, dB
+    global skyColorR, skyColorG, skyColorB
+    global skyR, skyG, skyB
+    global plus
 
     GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
 
@@ -100,8 +129,31 @@ def display(shader, vertex_array_object):
     GL.glUseProgram(0)
 
     GL.glUseProgram(shader[1])
+    redLoc = GL.glGetUniformLocation(shader[1], "r")
+    greenLoc = GL.glGetUniformLocation(shader[1], "g")
+    blueLoc = GL.glGetUniformLocation(shader[1], "b")
+
+    if(skyR < skyColorR) and (plus):
+        skyR = skyR + dR
+        skyG = skyG + dG
+        skyB = skyB + dB
+        GL.glUniform1f(redLoc, -skyR)
+        GL.glUniform1f(greenLoc, -skyG)
+        GL.glUniform1f(blueLoc, -skyB)
+        if (skyR >= skyColorR):
+            plus = False
+    else:
+        skyR = skyR - dR
+        skyG = skyG - dG
+        skyB = skyB - dB
+        GL.glUniform1f(redLoc, -skyR)
+        GL.glUniform1f(greenLoc, -skyG)
+        GL.glUniform1f(blueLoc, -skyB)
+        if (skyR <= skyDarkR):
+            plus = True
+
     GL.glBindVertexArray( vertex_array_object[1] )
-    GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
+    GL.glDrawArrays(GL.GL_POLYGON, 0, 4)
     GL.glBindVertexArray( 0 )
     GL.glUseProgram(0)
     
@@ -111,7 +163,7 @@ def main():
     global vertices 
 
     pygame.init()
-    screen = pygame.display.set_mode((512, 512), pygame.OPENGL|pygame.DOUBLEBUF)
+    screen = pygame.display.set_mode((800, 600), pygame.OPENGL|pygame.DOUBLEBUF)
     GL.glClearColor(0.5, 0.5, 0.5, 1.0)
     GL.glEnable(GL.GL_DEPTH_TEST)
 
@@ -120,20 +172,20 @@ def main():
         OpenGL.GL.shaders.compileShader(fragment_shader, GL.GL_FRAGMENT_SHADER)
     )
 
-    shader2 = OpenGL.GL.shaders.compileProgram(
+    shader_sky = OpenGL.GL.shaders.compileProgram(
         OpenGL.GL.shaders.compileShader(vertex_shader, GL.GL_VERTEX_SHADER),
-        OpenGL.GL.shaders.compileShader(fragment_shader2, GL.GL_FRAGMENT_SHADER)
+        OpenGL.GL.shaders.compileShader(fragment_shader_sky, GL.GL_FRAGMENT_SHADER)
     )
     shaders = []
     shaders.append(shader)
-    shaders.append(shader2)
+    shaders.append(shader_sky)
     
     clock = pygame.time.Clock()
     
     while True:
         vertex_array_object = []
         vertex_array_object.append(create_object(shader, vertices))
-        vertex_array_object.append(create_object(shader2, vertices2))     
+        vertex_array_object.append(create_object(shader_sky, background))     
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
